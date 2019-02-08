4d3079b33e681b25459d0478e006bcdf51898460
Peter Thorin <peter.thorin@gmail.com>
2019-02-08 10:28:55 +0100

    Add vim-plug as part of the install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/vimrc ~/.config/nvim/init.vim
