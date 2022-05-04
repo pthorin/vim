d84704951c31e61e3267f3bb9300e27f1766cfc8
Peter Thorin <pthorin@volvocars.com>
2022-05-04 07:57:39 +0200

    Create folder for nvim

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s `pwd`/vimrc ~/.vimrc
+mkdir -p ~/.config/nvim/
 ln -s `pwd`/vimrc ~/.config/nvim/init.vim
